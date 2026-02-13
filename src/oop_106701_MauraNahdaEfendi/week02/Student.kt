7cbabd874cc56147826a48b998c62e066565a3e1
maura nahda <mauranahda756@gmail.com>
2026-02-13 15:47:18 +0700

    week02: add warning validation in init block

--- src/oop_106701_MauraNahdaEfendi/week02/Student.kt
+++ src/oop_106701_MauraNahdaEfendi/week02/Student.kt
@@ -1,9 +1,13 @@
 package oop_106701_MauraNahdaEfendi.week02
 
-class Student (
-    val name: String,
-    val nim: String,
-    val major: String
-) {
-    //body class kosong dulu
+class Student (val name: String, val nim: String, val major: String){
+    init {
+        //validasi sederhana: cek panjang NIM
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data Mahasiswa $name mungkin akan bermasalah di sistem")
+        } else {
+            println("LOG: Objek student $name berhasil dialokasikan di memory")
+        }
+    }
 }
