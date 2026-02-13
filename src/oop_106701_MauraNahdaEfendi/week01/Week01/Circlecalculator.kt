14d9d4c3a190f46663d9c3395efee3426d434af6
maura nahda <mauranahda756@gmail.com>
2026-02-13 15:23:17 +0700

    week01: (tugas 1) clean up unused args and comments

--- src/oop_106701_MauraNahdaEfendi/week01/Week01/Circlecalculator.kt
+++ src/oop_106701_MauraNahdaEfendi/week01/Week01/Circlecalculator.kt
@@ -1,16 +1,14 @@
 package oop_106701_MauraNahdaEfendi.week01.Week01
 
-fun main(args: Array<String>) {
-// Variable definition
-    var radius: Double = 7.0
-    var pi: Double = 3.14
-// Calculation
-    var area: Double = pi * radius * radius
-    // Output Concatenation
+fun main() {
+    val radius = 7.0
+    val pi = 3.14
+    val area = pi * radius * radius
+
+    println("Radius: $radius, Area: $area")
     println(checkSize(area))
-// Logic check
-    checkSize(area)
 }
+
 fun checkSize(area: Double) =
     if (area > 100) "This is a Big Circle"
     else "This is a Small Circle"
