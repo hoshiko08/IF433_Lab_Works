23164f5865f2330660099ee70039984e9773eb95
maura nahda <mauranahda756@gmail.com>
2026-02-13 14:09:42 +0700

    week01: initial setup and procedural code

---
+++ src/oop_106701_MauraNahdaEfendi/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_106701_MauraNahdaEfendi.week01
+
+fun main () {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    //Concatenation gaya lama
+    print("Nama: " + name + ", Nilai:" + score)
+}
