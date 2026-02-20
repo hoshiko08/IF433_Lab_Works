76ecef6e8071dc63132aa1c7c7baa3e8790c753e
maura nahda <mauranahda756@gmail.com>
2026-02-20 13:47:28 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_106701_MauraNahdaEfendi/week03/Main.kt
+++ src/oop_106701_MauraNahdaEfendi/week03/Main.kt
@@ -2,12 +2,8 @@ package oop_106701_MauraNahdaEfendi.week03
 
 fun main() {
 
-    val weapon = Weapon("Excalibur")
+    val player = Player("Maura")
 
-    weapon.damage = -50
-    weapon.damage = 9999
-
-    println("Weapon: ${weapon.name}")
-    println("Damage: ${weapon.damage}")
-    println("Tier: ${weapon.tier}")
+    player.addXp(50)
+    player.addXp(60)
 }
