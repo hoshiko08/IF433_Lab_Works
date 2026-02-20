159515c1ba41cf1422f95d77b755493c79268724
maura nahda <mauranahda756@gmail.com>
2026-02-20 13:39:49 +0700

    week03: add private property

--- src/oop_106701_MauraNahdaEfendi/week03/Employee.kt
+++ src/oop_106701_MauraNahdaEfendi/week03/Employee.kt
@@ -11,4 +11,14 @@ class Employee {
                 field = value
             }
         }
+
+    private var performanceRating: Int = 0
+
+    fun updatePerformanceRating(newRating: Int) {
+        if (newRating in 1..5) {
+            performanceRating = newRating
+        } else {
+            println("Rating harus antara 1 sampai 5")
+        }
+    }
 }
