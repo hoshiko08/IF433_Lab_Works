7f0284e0c4f386ddb7eb98a3b92a642fd28636a5
maura nahda <mauranahda756@gmail.com>
2026-02-20 13:37:20 +0700

    week03: simulate stack overflow error in setter

--- src/oop_106701_MauraNahdaEfendi/week03/Employee.kt
+++ src/oop_106701_MauraNahdaEfendi/week03/Employee.kt
@@ -2,5 +2,9 @@ package oop_106701_MauraNahdaEfendi.week03
 
 class Employee {
     var name: String = ""
+
     var salary: Int = 0
+        set(value) {
+            this.salary = value // SALAH! menyebabkan infinite loop
+        }
 }
---
+++ src/oop_106701_MauraNahdaEfendi/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_106701_MauraNahdaEfendi.week03
+
+fun main() {
+    val e = Employee()
+    e.salary = 5000
+    println(e.salary)
+}
