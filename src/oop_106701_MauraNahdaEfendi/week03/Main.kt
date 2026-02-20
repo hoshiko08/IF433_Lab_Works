78db6751a839af1b9c2003be3df405d9ef80edf8
maura nahda <mauranahda756@gmail.com>
2026-02-20 13:40:57 +0700

    week03: test all encapsulation logic in main

--- src/oop_106701_MauraNahdaEfendi/week03/Main.kt
+++ src/oop_106701_MauraNahdaEfendi/week03/Main.kt
@@ -2,6 +2,14 @@ package oop_106701_MauraNahdaEfendi.week03
 
 fun main() {
     val e = Employee()
+    e.name = "Budi"
+
+    e.salary = -1000
     e.salary = 5000
-    println(e.salary)
+
+    e.updatePerformanceRating(4)
+
+    println("Nama: ${e.name}")
+    println("Salary: ${e.salary}")
+    println("Bonus: ${e.bonus}")
 }
