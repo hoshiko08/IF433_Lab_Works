a75fff1c66f2896a766add1f721ec25eb2d90efc
maura nahda <mauranahda756@gmail.com>
2026-03-13 15:28:08 +0700

    week06: fix error in  implement decoupled processCheckout in main

--- src/oop_106701_MauraNahdaEfendi/week06/Main.kt
+++ src/oop_106701_MauraNahdaEfendi/week06/Main.kt
@@ -2,23 +2,22 @@ package oop_106701_MauraNahdaEfendi.week06
 
 // Fungsi ini Decoupled! Tidak peduli kelas aslinya apa. [cite: 116]
 fun processCheckout(method: PaymentMethod, amount: Double) {
-    println("-> Memulai checkout...") [cite: 118]
+    println("-> Memulai checkout...")
     method.pay(amount) // Dynamic polymorphism in action [cite: 119]
 }
 
 fun main() {
     // Pengujian dari Latihan B & C
-    val myWatch = Smartwatch() [cite: 121]
-    myWatch.showTime() [cite: 122]
+    val myWatch = Smartwatch()
+    myWatch.showTime()
 
-    val myPhone = Smartphone() [cite: 123]
-    myPhone.turnon() [cite: 124]
-
+    val myPhone = Smartphone()
+    myPhone.turnon()
     // Pengujian dari Latihan E (Decoupling)
-    val pay1 = Gopay() [cite: 125]
-    val pay2 = CreditCard() [cite: 126, 127]
+    val pay1 = Gopay()
+    val pay2 = CreditCard()
 
-    println("\n=== TESTING CHECKOUT ===") [cite: 128]
-    processCheckout(method = pay1, amount = 50000.0) [cite: 129, 131]
-    processCheckout(method = pay2, amount = 150000.0) [cite: 130, 131]
+    println("\n=== TESTING CHECKOUT ===")
+    processCheckout(method = pay1, amount = 50000.0)
+    processCheckout(method = pay2, amount = 150000.0)
 }
