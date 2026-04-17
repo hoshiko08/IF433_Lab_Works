9c9a47ac2b35fcb8286de96027736948379177da
maura nahda <mauranahda756@gmail.com>
2026-04-17 15:20:12 +0700

    week08: create basic UserProfile with nullable fields

---
+++ src/oop_106701_MauraNahdaEfendi/week08/UserProfile.kt
@@ -0,0 +1,8 @@
+package oop_106701_MauraNahdaEfendi.week08
+
+data class UserProfile(
+    val id: String,
+    val name: String,
+    val email: String?,      // nullable
+    val phoneNumber: String? // nullable
+)
