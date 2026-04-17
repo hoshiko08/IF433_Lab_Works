3d50fb58e0782aedc7ee7a672fa20a0fd52c8d88
maura nahda <mauranahda756@gmail.com>
2026-04-17 15:40:32 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_106701_MauraNahdaEfendi/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_106701_MauraNahdaEfendi.week08;
+
+public class JavaPaymentService {
+
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
