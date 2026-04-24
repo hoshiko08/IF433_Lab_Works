e37fdd6bdf549a0b8145c0a7a13c99649941f611
maura nahda <mauranahda756@gmail.com>
2026-04-24 15:18:16 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_106701_MauraNahdaEfendi/week09/TradingDashboard.kt
@@ -0,0 +1,11 @@
+package oop_106701_MauraNahdaEfendi.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 15, 10.0, "OPEN"),
+        TradeLog("ETHUSDT", "LONG", 20, 7.5, "CLOSED"),
+        TradeLog("XRPUSDT", "SHORT", 5, -2.0, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 10, 3.0, "OPEN")
+    )
