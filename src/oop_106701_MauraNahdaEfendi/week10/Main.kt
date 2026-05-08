d651be1535331fa47c7d74ff6fd58afacd61a57e
maura nahda <mauranahda756@gmail.com>
2026-05-08 15:34:21 +0700

    week10: instantiate Box with Int and String types

---
+++ src/oop_106701_MauraNahdaEfendi/week10/Main.kt
@@ -0,0 +1,10 @@
+package oop_106701_MauraNahdaEfendi.week10
+
+fun main() {
+
+    val intBox = Box(100)
+    val stringBox = Box("Hello Kotlin")
+
+    println(intBox.getItem())
+    println(stringBox.getItem())
+}
