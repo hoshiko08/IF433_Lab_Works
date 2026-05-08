61135173288db0337b969516e11d545e1c9b1aba
maura nahda <mauranahda756@gmail.com>
2026-05-08 15:15:30 +0700

    week11: implement apply for object initialization

--- src/oop_106701_MauraNahdaEfendi/week11/ScopeConfiguration.kt
+++ src/oop_106701_MauraNahdaEfendi/week11/ScopeConfiguration.kt
@@ -5,3 +5,13 @@ class User {
     var name: String = ""
     var age: Int = 0
 }
+fun main() {
+
+    val user = User().apply {
+        name = "Maura"
+        age = 20
+    }
+
+    println(user.name)
+    println(user.age)
+}
