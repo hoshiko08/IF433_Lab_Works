61ab7c60a4b19440847af5cdf522b041c899c566
maura nahda <mauranahda756@gmail.com>
2026-05-08 15:44:34 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_106701_MauraNahdaEfendi/week10/WalletRepository.kt
@@ -0,0 +1,14 @@
+package oop_106701_MauraNahdaEfendi.week10
+
+class WalletRepository<T> {
+
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
