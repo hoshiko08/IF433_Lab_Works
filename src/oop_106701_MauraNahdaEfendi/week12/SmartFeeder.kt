fdcb7f4b6d8987270209bafe4416df55699c7a9d
maura nahda <mauranahda756@gmail.com>
2026-05-15 14:59:50 +0700

    week12: (task) setup SmartFeeder main function

--- src/oop_106701_MauraNahdaEfendi/week12/SmartFeeder.kt
+++ src/oop_106701_MauraNahdaEfendi/week12/SmartFeeder.kt
@@ -13,3 +13,7 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     println("Kibble berhasil dikeluarkan!")
     return availableGram - requestedGram
 }
+
+fun main() {
+    // 6. Main System Setup [cite: 150]
+    var currentKibbleStock = 50
