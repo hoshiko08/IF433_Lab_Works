06aafa441e377b05fdc003d3a03617ffc00e657a
maura nahda <mauranahda756@gmail.com>
2026-05-22 14:08:35 +0700

    week13: (task) implement loadTrades with FileNotFoundException handling

--- src/oop_106701_MauraNahdaEfendi/week13/CryptoTradeLogger.kt
+++ src/oop_106701_MauraNahdaEfendi/week13/CryptoTradeLogger.kt
@@ -1,6 +1,7 @@
 package oop_106701_MauraNahdaEfendi.week13
 
 import java.io.File
+import java.io.FileNotFoundException
 
 data class TradeRecord(
     val id: Int,
@@ -36,5 +37,15 @@ fun saveTrades(trades: List<TradeRecord>, path: String) {
         trades.forEach {
             writer.println(it.toCsv())
         }
+    }
+}
+
+fun loadTrades(path: String): List<TradeRecord> {
+    return try {
+        File(path).readLines().mapNotNull {
+            fromCsvTrade(it)
+        }
+    } catch (e: FileNotFoundException) {
+        emptyList()
     }
 }
